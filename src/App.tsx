0f8b4d5cea88f6f02769905b9a0837b9acf155c1
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-17 15:21:51 -0700

    add navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import logo from './logo.svg';
 import './App.css';
 import Labs from './Labs';
 import Kanbas from './Kanbas';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <HashRouter>
     <div>
-      <HashRouter>
-        <Routes>
-          <Route path="/labs" element={<Labs />} />
-          <Route path="/Kanbas" element={<Kanbas />} />
-        </Routes>
-        
-      </HashRouter>
-    </div>
+      <Link to="/Labs">Labs</Link> <Link to="/Kanbas">Kanbas</Link>
+      <Routes>
+        <Route path="/labs" element={<Labs />} />
+        <Route path="/Kanbas" element={<Kanbas />} />
+      </Routes>
+    </div> 
+    </HashRouter>
   );
 }
 
