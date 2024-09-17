39a0fc9f2a75ced7a2db31b2970c8db1df7d5a38
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-17 16:02:36 -0700

    move the links from app.tsx to Labs

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router"
 import Lab1 from "./Lab1"
 import Lab2 from "./Lab2"
 import Lab3 from "./Lab3"
+import TOC from "./TOC"
 
 export default function Labs(){
   return (
     <div>
       <h1>Dong Chen</h1>
+      <TOC />
       <h1>Labs</h1>
       <Routes>
         <Route path="Lab1" element={<Lab1 />} />
