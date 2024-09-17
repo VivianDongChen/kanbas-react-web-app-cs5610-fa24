12177f0a2feb8fb08aca46bc54c2e9931b6a837b
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-16 20:59:17 -0700

    add paragraph tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -26,17 +26,20 @@ export default function Lab1(){
         white spaces and render all the text as one single set of sentences. To
         force the browser to add vertical spacing, wrap the paragraphs you want to
         separate with the paragraph tag</p>
+        <p id="wd-p-2">
 This is the first paragraph. The paragraph tag is used to format
 vertical gaps between long pieces of text like this one.
-
+        </p>
+        <p id="wd-p-3">
 This is the second paragraph. Even though there is a deliberate white
 gap between the paragraph above and this paragraph, by default
 browsers render them as one contiguous piece of text as shown here on
 the right.
-
+        </p>
+        <p id="wd-p-4">
 This is the third paragraph. Wrap each paragraph with the paragraph
 tag to tell browsers to render the gaps.
-
+        </p>
       </div>
         <p id="wd-p-1">
         This is a paragraph. We often separate a long set of sentences with
