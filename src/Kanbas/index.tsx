d0795781bf2a872317f7806694565ae863db60d6
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-18 13:47:39 -0700

    add courses into Kanbas and links to courses

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "./Account";
 import Dashboard from "./DashBoard";
 import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
 
 export default function Kanbas() {
   return (
@@ -17,6 +18,9 @@ export default function Kanbas() {
               <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Courses/:cid/*" element={<Courses />} />
+              <Route path="/Calendar" element={<h1>Calendar</h1>} />
+              <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
           </td>
         </tr>
