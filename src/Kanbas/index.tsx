234e01248648b2ac4e7edc39be8a95da2b884f48
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-18 12:58:49 -0700

    add navigations to Kanbas screen

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,16 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "./Account";
 import Dashboard from "./DashBoard";
+import KanbasNavigation from "./Navigation";
 
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <h1>Kanbas</h1>
-      <Routes>
-        <Route path="/" element={<Navigate to="Account" />} />
-        <Route path="/Account/*" element={<Account />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-      </Routes>
+      <table>
+        <tr>
+          <td valign="top">
+            <KanbasNavigation />
+          </td>
+          <td valign="top">
+            <h1>Kanbas</h1>
+            <Routes>
+              <Route path="/" element={<Navigate to="Account" />} />
+              <Route path="/Account/*" element={<Account />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
