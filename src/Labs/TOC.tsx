8f447683831113167d80fffb31d7fe16fa4f157c
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-18 20:04:19 -0700

    add github address to TOC.tsx

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,5 +7,6 @@ export default function TOC() {
  <li><Link id="wd-a2" to="/Labs/Lab2">Lab 2</Link></li>
  <li><Link id="wd-a3" to="/Labs/Lab3">Lab 3</Link></li>
  <li><Link id="wd-kanbas" to="/Kanbas">Kanbas</Link></li>
+ <li><a href="https://github.com/VivianDongChen/kanbas-react-web-app-cs5610-fa24.git" id="wd-github" >GitHub</a></li>
  </ul>
 );}
