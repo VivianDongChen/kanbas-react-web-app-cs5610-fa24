57f04f10e2295f25c1c5f2b40eac2e18a1c445da
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-18 17:39:00 -0700

    edit the route of Assignments

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -19,12 +19,12 @@ export default function Courses() {
               <Route path="/" element={<Navigate to="Home" />} />
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
-              <Route path="Assignments" element={<h2>Assignments</h2>} />
+              <Route path="Assignments" element={<Assignments />} />
               <Route
                 path="Assignments/:aid"
                 element={<h2>Assignment Editor</h2>}
               />
-              <Route path="People"  element={<Assignments />} />
+              <Route path="People"  element={<h3>People</h3>} />
             </Routes>
           </td>
         </tr>
