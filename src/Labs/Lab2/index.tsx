c6ac8d864b62eb24983f9fed55877311f131d7b0
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-20 17:17:14 -0700

    3.1.3 select content with ID selectors

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -10,6 +11,17 @@ export default function Lab2() {
         Although it's very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+          Instead of changing the look and feel of all the elements of the same
+          name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+          Here's another paragraph using a different ID and a different look and
+          feel
+        </p>
+      </div>
     </div>
   );
 }
