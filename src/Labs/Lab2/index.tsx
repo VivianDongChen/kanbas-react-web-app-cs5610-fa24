81727452d13985732d313121ff49922865091c71
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-21 23:42:56 -0700

    3.1.17 float images and content

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -264,7 +264,7 @@ export default function Lab2() {
           </div>
           <div
             className="wd-pos-absolute-120-20
-wd-bg-color-red wd-dimension-square"
+            wd-bg-color-red wd-dimension-square"
           >
             Square
           </div>
@@ -276,6 +276,25 @@ wd-bg-color-red wd-dimension-square"
         <br />
         <br />
         <br />
+      </div>
+      <div id="wd-float-divs">
+        <h2>Float</h2>
+        <div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+            Yellow{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+            Blue{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+            Red{" "}
+          </div>
+          <img
+            className="wd-float-right"
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          />
+          <div className="wd-float-done"></div>
+        </div>
       </div>
     </div>
   );
