fa960d4c826f194f062f548284344ea8e2faa874
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-21 23:21:55 -0700

    3.1.13 style relative position

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -183,6 +183,25 @@ export default function Lab2() {
           <div className="wd-dimension-square wd-bg-color-red">Square</div>
         </div>
       </div>
+      <div id="wd-css-position-relative">
+        <h2>Relative</h2>
+        <div className="wd-bg-color-gray">
+          <div
+            className="wd-bg-color-yellow
+            wd-dimension-portrait"
+          >
+            <div className="wd-pos-relative-nudge-down-right">Portrait</div>
+          </div>
+          <div
+            className="wd-pos-relative-nudge-up-right
+            wd-bg-color-blue wd-fg-color-white
+            wd-dimension-landscape"
+          >
+            Landscape
+          </div>
+          <div className="wd-bg-color-red wd-dimension-square">Square</div>
+        </div>
+      </div>
     </div>
   );
 }
