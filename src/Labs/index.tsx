90a6a9a682a006f3820cf879dd4519af5b40b33b
VivianDongChen <chen.dong1@northeastern.edu>
2024-09-22 22:26:59 -0700

    add section next to the name

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ import Lab3 from "./Lab3"
 export default function Labs(){
   return (
     <div>
-      <h1>Dong Chen</h1>
+      <h1>Dong Chen - SEC 02</h1>
       <TOC />
       <h1>Labs</h1>
       <Routes>
