d1f4130c7b57b4e39aa6c4b2d42dc4ac636687c2
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 12:44:34 -0700

    modify kanbas/navigation

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -38,28 +38,30 @@ import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 export default function KanbasNavigation() {
-return (
-<div id="wd-kanbas-navigation" style={{ width: 120 }}
-className="list-group rounded-0">
-<a id="wd-neu-link" target="_blank"
-href="https://www.northeastern.edu/"
-className="list-group-item bg-black border-0">
-<img src="/images/NEU.png" width="75px" /></a><br />
-<Link to="/Kanbas/Account" id="wd-account-link"
-className="list-group-item text-white
-bg-black text-center border-0">
-<FaRegCircleUser className="fs-1 text text-white" /><br />
-Account </Link><br />
-<Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
-className="list-group-item text-center border-0
-bg-white text-danger">
-<AiOutlineDashboard className="fs-1 text-danger" /><br />
-Dashboard </Link><br />
-<Link to="/Kanbas/Dashboard" id="wd-course-link"
-className="list-group-item text-white
-bg-black text-center border-0">
-<LiaBookSolid className="fs-1 text-danger" /><br />
-Courses </Link><br />
-{/* complete styling the rest of the links */}
-</div>
-);}
+  return (
+    <div
+      id="wd-kanbas-navigation"
+      style={{ width: 120 }}
+      className="list-group rounded-0"
+    >
+      <a id="wd-neu-link"
+        target="_blank"
+        href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0">
+        <img src="/images/NEU.png" width="75px" /></a>
+      <Link to="/Kanbas/Account" id="wd-account-link"
+        className="list-group-item text-white bg-black text-center border-0">
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account</Link>
+      <Link to="/Kanbas/Dashboard" id="wd-dashboard-link"
+        className="list-group-item text-center border-0 bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard</Link>
+      <Link to="/Kanbas/Dashboard" id="wd-course-link"
+        className="list-group-item text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses</Link><br />
+      {/* complete styling the rest of the links */}
+    </div>
+  );
+}
