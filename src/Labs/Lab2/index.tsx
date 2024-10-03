55c8f0f5eb61b4e79ccc2f224bfc8a92f5f2bb63
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-02 18:41:42 -0700

    3.3.6 style tables

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -14,6 +14,7 @@ import Flex from "./Flex";
 import Dimensions from "./Dimensions";
 import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
 
 export default function Lab2() {
   return (
@@ -86,6 +87,7 @@ export default function Lab2() {
         <ReactIcons />
         <BootstrapGrids />
         <ScreenSizeLabel />
+        <BootstrapTables />
       </div>
     </div>
   );
