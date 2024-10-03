1f66ac8951a9e458d6a49402b43ccc41ebda6f6a
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-02 20:03:29 -0700

    3.3.15 responsive forms

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -100,6 +100,42 @@ export default function BootstrapForms() {
           <span className="input-group-text">0.00</span>
         </div>
       </div>
+      <div id="wd-css-responsive-forms-1">
+        <h3>Responsive forms</h3>
+        <div className="mb-3 row">
+          <label htmlFor="email1" className="col-sm-2 col-form-label">
+            Email
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+              id="email1"
+              value="email@example.com"
+            />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label htmlFor="password1" className="col-sm-2 col-form-label">
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input type="password" id="password1" className="form-control" />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label htmlFor="textarea2" className="col-sm-2 col-form-label">
+            Bio
+          </label>
+          <div className="col-sm-10">
+            <textarea
+              className="form-control"
+              id="textarea2"
+              rows={3}
+            ></textarea>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
