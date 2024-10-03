e7739a0b4cf16c5f38cc0d347872ed4a862c58f5
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-02 21:49:15 -0700

    add the handleTabClick function, you can prevent the default anchor click behavior and maintain the link structure without triggering any navigation.

--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,25 +1,31 @@
 export default function BootstrapNavigation() {
+
+    const handleTabClick = (event: { preventDefault: () => void; }) => {
+        event.preventDefault(); // Prevent default anchor click behavior
+        // Add your tab-switching logic here
+      };
+
   return (
     <div id="wd-css-navigating-with-tabs">
       <h2>Tabs</h2>
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a className="nav-link active" href="javascript:void(0)">
+          <a className="nav-link active" href="#">
             Active
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="javascript:void(0)">
+          <a className="nav-link" href="#">
             Link
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="javascript:void(0)">
+          <a className="nav-link" href="#">
             Link
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="javascript:void(0)">
+          <a className="nav-link disabled" href="#">
             Disabled
           </a>
         </li>
