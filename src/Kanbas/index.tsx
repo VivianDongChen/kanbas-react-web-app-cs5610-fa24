538332b75804c72b42b7cba3452c4b58bc6a4453
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 14:41:01 -0700

    create class wd-main-content-offset to move the route to the right off under sidebar

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,20 +3,18 @@ import Account from "./Account";
 import Dashboard from "./DashBoard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import ".styles.css";
 
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      {/* <h1>Kanbas</h1> */}
       <KanbasNavigation />
-      <div>
+      <div className="wd-main-content-offset p-3">
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="/Account/*" element={<Account />} />
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Courses/:cid/*" element={<Courses />} />
-          {/* <Route path="/Calendar" element={<h1>Calendar</h1>} />
-          <Route path="/Inbox" element={<h1>Inbox</h1>} /> */}
         </Routes>
       </div>     
     </div>
