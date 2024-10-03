2fb6a8b557e90254d9aad91f2056b8241fcbc2d6
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 12:48:37 -0700

    import link

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,4 +1,4 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // export default function KanbasNavigation() {
 //   return (
 //     <div id="wd-kanbas-navigation">
