3b647a6bab5b95971b7ec79c02006b0b7b91fd54
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-02 19:48:01 -0700

    3.3.13 style range and sliders

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -73,6 +73,20 @@ export default function BootstrapForms() {
           </label>
         </div>
       </div>
+      <div id="wd-css-styling-range-and-sliders">
+        <h3>Range</h3>
+        <label htmlFor="range1" className="form-label">
+          Example range
+        </label>
+        <input
+          type="range"
+          className="form-range"
+          min="0"
+          max="5"
+          step="0.5"
+          id="range1"
+        />
+      </div>
     </>
   );
 }
