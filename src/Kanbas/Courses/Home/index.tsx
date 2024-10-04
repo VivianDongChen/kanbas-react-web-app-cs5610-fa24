f6faa3c4370de68806119204f232608a1bb6d977
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-04 00:33:52 -0700

    4.6

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <div className="flex-fill">
         <Modules />
       </div>
+      <div className="mx-3"></div>
       <div className="d-none d-md-block">
         <CourseStatus />
       </div>
