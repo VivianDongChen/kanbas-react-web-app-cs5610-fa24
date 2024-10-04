aaf57a3513e3925e2147519d5fd5c808aa3b8627
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-04 05:11:38 -0700

    4.9

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,33 +1,72 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 export default function Profile() {
+  const [profile, setProfile] = useState({
+    username: "alice",
+    password: "123",
+    firstName: "Alice",
+    lastName: "Wonderland",
+    dob: "", // Date of Birth field
+    email: "alice@wonderland.com",
+    role: "User"
+  });
+
   return (
-    <div id="wd-profile-screen">
-      <h3>Profile</h3>
-      <input id="wd-username" value="alice" placeholder="username" />
-      <br />
+    <div id="wd-profile-screen" className="container mt-4">
+      <h1>Profile</h1>
       <input
-        id="wd-password"
-        value="123"
-        placeholder="password"
-        type="password"
+        id="wd-username"
+        placeholder={profile.username}
+        className="form-control mb-2"
+        value={profile.username}
       />
-      <br />
-      <input id="wd-firstname" value="Alice" placeholder="First Name" />
-      <br />
-      <input id="wd-lastname" value="Wonderland" placeholder="Last Name" />
-      <br />
-      <input id="wd-dob" value="2000-01-01" type="date" />
-      <br />
-      <input id="wd-email" value="alice@wonderland" type="email" />
-      <br />
-      <select id="wd-role">
-        <option value="USER">User</option>
-        <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option>
-        <option value="STUDENT">Student</option>
-      </select>
-      <br />
-      <Link to="/Kanbas/Account/Signin">Sign out</Link>
+      <input
+        type="password"
+        id="wd-password"
+        placeholder={profile.password}
+        className="form-control mb-2"
+        value={profile.password}
+      />
+      <input
+        id="wd-first-name"
+        placeholder={profile.firstName}
+        className="form-control mb-2"
+        value={profile.firstName}
+      />
+      <input
+        id="wd-last-name"
+        placeholder={profile.lastName}
+        className="form-control mb-2"
+        value={profile.lastName}
+      />
+      <input
+        type="date"
+        id="wd-dob"
+        placeholder="mm/dd/yyyy"
+        className="form-control mb-2"
+        value={profile.dob}
+      />
+      <input
+        type="email"
+        id="wd-email"
+        placeholder={profile.email}
+        className="form-control mb-2"
+        value={profile.email}
+      />
+      <input
+        id="wd-role"
+        placeholder={profile.role}
+        className="form-control mb-2"
+        value={profile.role}
+      />
+      <Link
+        id="wd-profile-btn"
+        to="/Kanbas/Account/Signin"
+        className="btn btn-danger w-100"
+      >
+        Signout
+      </Link>
     </div>
   );
 }
