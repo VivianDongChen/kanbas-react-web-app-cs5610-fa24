cc5a6ebafd9d8d2d05539831c8def3cdad648864
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-04 05:42:20 -0700

    4.9

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,16 +1,3 @@
-// import { Link } from "react-router-dom";
-// export default function TOC() {
-//  return (
-//  <ul>
-//  <li><Link id="wd-a" to="/Labs">Labs</Link></li>
-//  <li><Link id="wd-a1" to="/Labs/Lab1">Lab 1</Link></li>
-//  <li><Link id="wd-a2" to="/Labs/Lab2">Lab 2</Link></li>
-//  <li><Link id="wd-a3" to="/Labs/Lab3">Lab 3</Link></li>
-//  <li><Link id="wd-kanbas" to="/Kanbas">Kanbas</Link></li>
-//  <li><a href="https://github.com/VivianDongChen/kanbas-react-web-app-cs5610-fa24.git" id="wd-github" >GitHub</a></li>
-//  </ul>
-// );}
-
 import { useLocation } from "react-router";
 export default function TOC() {
 const { pathname } = useLocation();
@@ -45,7 +32,7 @@ Kanbas
 </a>
 </li>
 <li className="nav-item">
-<a id="wd-k" href="https://github.com/VivianDongChen/kanbas-react-web-app-cs5610-fa24.git" className="nav-link">
+<a id="wd-github" href="https://github.com/VivianDongChen/kanbas-react-web-app-cs5610-fa24.git" className="nav-link">
 My GitHub
 </a>
 </li>
