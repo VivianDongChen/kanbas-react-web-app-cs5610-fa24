d5d912c1f449a485e51576e01ab728bd610f7bf4
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 21:29:59 -0700

    4.6

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ModuleControls from "./ModuleControls";
+import AssignmentControlButtons from "./AssignmentControlButtons";
+import { BsGripVertical } from "react-icons/bs";
 
 export default function Modules() {
   return (
@@ -9,6 +11,23 @@ export default function Modules() {
       <br />
       <br />
       <br />
+      <ul id="wd-modules" className="list-group rounded-0">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary"><BsGripVertical className="me-2 fs-3" /> ASSIGNMENTS <AssignmentControlButtons /></div>
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />
+              A1
+              <AssignmentControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />
+              A2
+              <AssignmentControlButtons /></li>
+            <li className="wd-lesson list-group-item p-3 ps-1"><BsGripVertical className="me-2 fs-3" />
+              A3
+              <AssignmentControlButtons /></li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 }
