350eacafe08b14f73e13a93bea8c66a6e7ef5f06
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 19:20:02 -0700

    4.5

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,7 +1,9 @@
 import { MdDoNotDisturbAlt } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
-import { BiImport } from "react-icons/bi";
+import { BiImport, BiHome, BiBarChart } from "react-icons/bi";
 import { LiaFileImportSolid } from "react-icons/lia";
+import { AiOutlineNotification } from "react-icons/ai";
+import { MdOutlineAnnouncement } from "react-icons/md";
 
 export default function CourseStatus() {
   return (
@@ -23,8 +25,29 @@ export default function CourseStatus() {
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
         <BiImport className="me-2 fs-5" /> Import Existing Content
       </button>
+      <br />
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
         <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons
+      </button>
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiHome className="me-2 fs-5" /> Choose Home Page
+      </button>
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiBarChart className="me-2 fs-5" /> View course Screen
+      </button>
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <MdOutlineAnnouncement className="me-2 fs-5" /> New Announcement
+      </button>
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiBarChart className="me-2 fs-5" /> New Analytics
+      </button>
+      <br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <AiOutlineNotification className="me-2 fs-5" /> View Course Notifications
       </button>
     </div>
   );
