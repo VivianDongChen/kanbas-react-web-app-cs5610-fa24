e53148828a36b7b605e677c03c6de637a7554bfb
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-03 18:50:38 -0700

    4.4

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -13,13 +13,13 @@ export default function Modules() {
           <div className="wd-title p-3 ps-2 bg-secondary"> Week 1 </div>
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
-              LEARNING OBJECTIVES{" "}
+              LEARNING OBJECTIVES
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1">
-              Introduction to the course{" "}
+              Introduction to the course
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1">
-              Learn what is Web Development{" "}
+              Learn what is Web Development
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
             <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
@@ -29,13 +29,13 @@ export default function Modules() {
           <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
-              LEARNING OBJECTIVES{" "}
+              LEARNING OBJECTIVES
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 1 </li>
             <li className="wd-lesson list-group-item p-3 ps-1"> LESSON 2 </li>
           </ul>
         </li>
-      </ul>{" "}
+      </ul>
     </div>
   );
 }
