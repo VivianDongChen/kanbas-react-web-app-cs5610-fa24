a2e6af89b737be8730a2da86cdd3723549f2dff6
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-18 14:49:46 -0700

    1

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -27,7 +27,7 @@ export default function Courses() {
             <Route path="Home" element={<Home />} />
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
-            <Route path=":cid/AssignmentEditor/:aid" element={<AssignmentEditor />} />
+            <Route path="/Kanbas/Courses/:cid/AssignmentEditor/:aid" element={<AssignmentEditor />} />
             <Route path="People" element={<PeopleTable />} />
           </Routes>
         </div>
