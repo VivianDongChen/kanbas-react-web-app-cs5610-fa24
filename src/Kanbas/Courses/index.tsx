694d5674b25378ecfb0445cb0452d2faf2666928
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-17 17:08:35 -0700

    1

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -13,7 +13,7 @@
 //       <h2 className="text-danger">
 //         <FaAlignJustify className="me-4 fs-4 mb-1" />
 //         Course 1234 </h2> <hr />
-//       
+//
 //       </div>
 //     </div>
 //   );
@@ -34,28 +34,26 @@ export default function Courses() {
   const { pathname } = useLocation();
   return (
     <div id="wd-courses">
-      <div className="flex-fill">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
         {course && course.name} &gt; {pathname.split("/")[4]}
       </h2>
       <div className="d-flex">
-       <div className="d-none d-md-block">
-         <CoursesNavigation />
-       </div>
-      </div>
-      <div className="flex-fill">
-        <Routes>
-          <Route path="Home" element={<Home />} />
-          <Route path="Modules" element={<Modules />} />
-          <Route path="Assignments" element={<Assignments />} />
-          <Route
-            path="AssignmentEditor/:assignmentId"
-            element={<AssignmentEditor />}
-          />
-          <Route path="People" element={<PeopleTable />} />
-        </Routes>
-      </div>   
+        <div className="d-none d-md-block">
+          <CoursesNavigation />
+        </div>
+        <div className="flex-fill">
+          <Routes>
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route
+              path="AssignmentEditor/:assignmentId"
+              element={<AssignmentEditor />}
+            />
+            <Route path="People" element={<PeopleTable />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
