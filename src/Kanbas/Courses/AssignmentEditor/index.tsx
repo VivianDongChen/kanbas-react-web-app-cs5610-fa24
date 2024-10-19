b434076ffb2e890e18c5cef9ebc4bb2bf6b2dcea
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-18 17:11:07 -0700

    1

--- src/Kanbas/Courses/AssignmentEditor/index.tsx
+++ src/Kanbas/Courses/AssignmentEditor/index.tsx
@@ -123,6 +123,7 @@ export default function AssignmentEditor() {
         <div className="col-sm-10 border p-3">
           <div className="form-group">
             <label>Assign to</label>
+            <select className="form-control"></select>
           </div>
           <div className="form-group">
             <label>Due</label>
