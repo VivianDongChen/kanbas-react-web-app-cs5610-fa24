69c3847decb70486c13fcb978c971671c39ed0dc
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-25 22:52:10 -0700

    2.2.2

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,10 +1,12 @@
 import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 
 export default function Lab4() {
     return(
         <div>
             <h2>Lab4</h2>
-            <ClickEvent />  
+            <ClickEvent /> 
+            <PassingDataOnEvent/> 
         </div>
         
     )
