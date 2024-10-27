b5547565c06bbb7193b45ae5b660de12314c35d8
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-26 17:17:32 -0700

    2.4.2

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,26 +1,29 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Lab1 from "./Lab1"
-import Lab2 from "./Lab2"
-import TOC from "./TOC"
-import Lab3 from "./Lab3"
-import Lab4 from "./Lab4"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import TOC from "./TOC";
+import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 
-export default function Labs(){
+export default function Labs() {
   return (
-    <div>
-      <h1>Dong Chen - SEC 02</h1>
-      <div className="p-3">
-        <TOC />
-      <h1>Labs</h1>
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        {/* <Route path="Lab3" element={<Lab3 />} /> */}
-        <Route path="Lab3/*" element={<Lab3 />} />
-        <Route path="Lab4/*" element={<Lab4 />} />
-      </Routes>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h1>Dong Chen - SEC 02</h1>
+        <div className="p-3">
+          <TOC />
+          <h1>Labs</h1>
+          <Routes>
+            <Route path="/" element={<Navigate to="Lab1" />} />
+            <Route path="Lab1" element={<Lab1 />} />
+            <Route path="Lab2" element={<Lab2 />} />
+            <Route path="Lab3/*" element={<Lab3 />} />
+            <Route path="Lab4/*" element={<Lab4 />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  )
+    </Provider>
+  );
 }
