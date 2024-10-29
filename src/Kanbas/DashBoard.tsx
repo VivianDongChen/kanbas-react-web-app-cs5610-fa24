d8e5fadf4b6aae0e212b43c9459f4f314c3da41f
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-29 12:45:31 -0700

    1

--- src/Kanbas/DashBoard.tsx
+++ src/Kanbas/DashBoard.tsx
@@ -1,55 +1,85 @@
 import { Link } from "react-router-dom";
 import "./Dashboard.css";
 import * as db from "./Database";
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function Dashboard() {
   // const courses = db.courses;
   const [courses, setCourses] = useState<any[]>(db.courses);
 
   const [course, setCourse] = useState<any>({
-    _id: "0", name: "New Course", number: "New Number",
-    startDate: "2023-09-10", endDate: "2023-12-15",
-    image: "/images/reactjs.jpg", description: "New Description"
-    });
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
+  });
 
   const addNewCourse = () => {
-    const newCourse = { ...course,
-    _id: new Date().getTime().toString() };
-    setCourses([...courses, { ...course, ...newCourse }]);
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString(),
+      image: course.image || "/images/react.jpg",
     };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
 
   return (
-    <div className = "p-4" id="wd-dashboard">
+    <div className="p-4" id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h5>
+        New Course
+        <button
+          className="btn btn-primary float-end"
+          id="wd-add-new-course-click"
+          onClick={addNewCourse}
+        >
+          {" "}
+          Add{" "}
+        </button>
 
-      <h5>New Course
-      <button className="btn btn-primary float-end"
-      id="wd-add-new-course-click"
-      onClick={addNewCourse} > Add </button>
-      </h5><br/>  
-      
-      <input defaultValue={course.name} className="form-control mb-2" 
-          onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
-      <textarea defaultValue={course.description} className="form-control"
-          onChange={(e) => setCourse({ ...course, description: e.target.value }) }/><hr />
+        
+      </h5>
+      <br />
+      <input
+        defaultValue={course.name}
+        className="form-control mb-2"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <textarea
+        defaultValue={course.description}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, description: e.target.value })}
+      />
+      <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Enter Image URL (optional)"
+        value={course.image}
+        onChange={(e) => setCourse({ ...course, image: e.target.value })}
+      />
+      <hr />
 
-      <h2 id="wd-dashboard-published">
-        Published Courses ({courses.length})
-      </h2>
+
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
       <hr />
       <div className="row" id="wd-dashboard-courses">
-      <div className="row row-cols-1 row-cols-md-5 g-4">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: "300px" }}>
-            <div className="card">
-
-
+              <div className="card">
                 <Link
                   to={`/Kanbas/Courses/${course._id}/Home`}
                   className="wd-dashboard-course-link text-decoration-none text-dark"
                 >
-                  <img src={course.image} alt={course.name} width="100%" height={160} />
+                  <img
+                    src={course.image}
+                    alt={course.name}
+                    width="100%"
+                    height={160}
+                  />
                   <div className="card-body">
                     <h5 className="wd-dashboard-course-title card-title">
                       {course.name}{" "}
