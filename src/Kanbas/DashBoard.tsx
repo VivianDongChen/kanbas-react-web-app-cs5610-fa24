0f0b30180ae8ff9447460fb270f481e2a622c680
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-29 11:16:39 -0700

    3.1.0, 3.1.1

--- src/Kanbas/DashBoard.tsx
+++ src/Kanbas/DashBoard.tsx
@@ -1,21 +1,46 @@
 import { Link } from "react-router-dom";
 import "./Dashboard.css";
 import * as db from "./Database";
+import React,{useState} from "react";
 
 export default function Dashboard() {
-  const courses = db.courses;
+  // const courses = db.courses;
+  const [courses, setCourses] = useState<any[]>(db.courses);
+
+  const course: any = {
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg", description: "New Description"
+    // create a course object with default values
+    };
+
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+    _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+    };
+
   return (
-    <div id="wd-dashboard">
+    <div className = "p-4" id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+
+      <h5>New Course
+      <button className="btn btn-primary float-end"
+      id="wd-add-new-course-click"
+      onClick={addNewCourse} > Add </button>
+      </h5><hr />
+      
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
-      </h2>{" "}
+      </h2>
       <hr />
-      <div id="wd-dashboard-courses" className="row">
-        <div className="row row-cols-1 row-cols-md-5 g-4">
+      <div className="row" id="wd-dashboard-courses">
+      <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
-            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-              <div className="card rounded-3 overflow-hidden">
+            <div key={course._id} className="col" style={{ width: "300px" }}>
+            <div className="card">
+
+
                 <Link
                   to={`/Kanbas/Courses/${course._id}/Home`}
                   className="wd-dashboard-course-link text-decoration-none text-dark"
