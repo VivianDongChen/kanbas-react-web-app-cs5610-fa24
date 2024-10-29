37f83c1c299f77a7b73cef65c832661dc95bbf53
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-29 11:34:11 -0700

    3.1.1

--- src/Kanbas/DashBoard.tsx
+++ src/Kanbas/DashBoard.tsx
@@ -7,12 +7,11 @@ export default function Dashboard() {
   // const courses = db.courses;
   const [courses, setCourses] = useState<any[]>(db.courses);
 
-  const course: any = {
+  const [course, setCourse] = useState<any>({
     _id: "0", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
     image: "/images/reactjs.jpg", description: "New Description"
-    // create a course object with default values
-    };
+    });
 
   const addNewCourse = () => {
     const newCourse = { ...course,
@@ -28,8 +27,13 @@ export default function Dashboard() {
       <button className="btn btn-primary float-end"
       id="wd-add-new-course-click"
       onClick={addNewCourse} > Add </button>
-      </h5><hr />
+      </h5><br/>  
       
+      <input defaultValue={course.name} className="form-control mb-2" 
+          onChange={(e) => setCourse({ ...course, name: e.target.value }) }/>
+      <textarea defaultValue={course.description} className="form-control"
+          onChange={(e) => setCourse({ ...course, description: e.target.value }) }/><hr />
+
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
       </h2>
