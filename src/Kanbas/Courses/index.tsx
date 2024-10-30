78384d35f2e10547ba4316f573c201d8fc8112e2
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-30 00:13:14 -0700

    1

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,7 @@ import { FaAlignJustify } from "react-icons/fa6";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import CoursesNavigation from "./Navigation";
+import ProtectedRoute1 from "../Account/ProtectedRoute1";
 
 export default function Courses({ courses }: { courses: any[]; }) {
   const { cid } = useParams();
@@ -26,7 +27,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
             <Route path="Home" element={<Home />} />
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
-            <Route path="AssignmentEditor/:aid" element={<AssignmentEditor />} />
+            <ProtectedRoute1><Route path="AssignmentEditor/:aid" element={<AssignmentEditor />} /></ProtectedRoute1>
             <Route path="People" element={<PeopleTable />} />
           </Routes>
         </div>
