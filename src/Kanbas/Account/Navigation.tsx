dd6f96f03ea4e2a85f6520e9588b8f8d2258064b
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-30 12:39:56 -0700

    1

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -7,33 +7,39 @@ export default function AccountNavigation() {
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   return (
     <div id="wd-account-navigation">
-      <Link
-        to={`/Kanbas/Account/Signin`}
-        id="wd-account-signin-link"
-        className="list-group-item active border border-0"
-      >
-        {" "}
-        Signin{" "}
-      </Link>{" "}
+
+      {links.includes("Signin") && (
+        <Link
+          to={`/Kanbas/Account/Signin`}
+          id="wd-account-signin-link"
+          className="list-group-item active border border-0"
+        >
+          Signin
+        </Link>
+      )}
       <br />
-      <Link
-        to={`/Kanbas/Account/Signup`}
-        id="wd-account-signup-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Signup{" "}
-      </Link>{" "}
+
+      {links.includes("Signup") && (
+        <Link
+          to={`/Kanbas/Account/Signup`}
+          id="wd-account-signup-link"
+          className="list-group-item text-danger border border-0"
+        >
+          Signup
+        </Link>
+      )}
       <br />
-      <Link
-        to={`/Kanbas/Account/Profile`}
-        id="wd-account-profile-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Profile{" "}
-      </Link>{" "}
-      <br />
+
+      {links.includes("Profile") && (
+        <Link
+          to={`/Kanbas/Account/Profile`}
+          id="wd-account-profile-link"
+          className="list-group-item text-danger border border-0"
+        >
+          Profile
+        </Link>
+      )}
+      
     </div>
   );
 }
