e713daeeae01ab8ae371d44caf976d6286d1f6cd
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-30 12:26:51 -0700

    3.4.5

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,17 +1,39 @@
 import { Link } from "react-router-dom";
 import "../styles.css";
+import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   return (
     <div id="wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`} id="wd-account-signin-link"
-      className="list-group-item active border border-0"> Signin </Link> <br />
-      <Link to={`/Kanbas/Account/Signup`} id="wd-account-signup-link"
-      className="list-group-item text-danger border border-0"> Signup </Link> <br />
-      <Link to={`/Kanbas/Account/Profile`} id="wd-account-profile-link"
-      className="list-group-item text-danger border border-0"> Profile </Link> <br />
+      <Link
+        to={`/Kanbas/Account/Signin`}
+        id="wd-account-signin-link"
+        className="list-group-item active border border-0"
+      >
+        {" "}
+        Signin{" "}
+      </Link>{" "}
+      <br />
+      <Link
+        to={`/Kanbas/Account/Signup`}
+        id="wd-account-signup-link"
+        className="list-group-item text-danger border border-0"
+      >
+        {" "}
+        Signup{" "}
+      </Link>{" "}
+      <br />
+      <Link
+        to={`/Kanbas/Account/Profile`}
+        id="wd-account-profile-link"
+        className="list-group-item text-danger border border-0"
+      >
+        {" "}
+        Profile{" "}
+      </Link>{" "}
+      <br />
     </div>
   );
 }
-
-
--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -3,8 +3,10 @@ import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import AccountNavigation from "./Navigation";
+import { useSelector } from "react-redux";
 
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div id="wd-account-screen" className="container-fluid">
       <div className="row">
@@ -13,7 +15,7 @@ export default function Account() {
         </div>
         <div className="col-md-3 offset-md-1"> 
           <Routes>
-            <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+            <Route path="/" element={<Navigate to={ currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin" }/>} />
             <Route path="/Signin" element={<Signin />} />
             <Route path="/Profile" element={<Profile />} />
             <Route path="/Signup" element={<Signup />} />
