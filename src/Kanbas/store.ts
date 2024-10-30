70f1a74680bba4131c2d46c2bfb7945249d48fc0
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-29 21:32:33 -0700

    3.4.2

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,6 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
+import accountReducer from "./Account/reducer";
 
-const store = configureStore({ reducer: { modulesReducer } });
+const store = configureStore({ reducer: { 
+    modulesReducer, 
+    accountReducer,
+},
+ });
 
 export default store;
