22326496f4d6901839556d11061ac563c2ddd6ba
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-29 19:43:38 -0700

    1

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -54,8 +54,8 @@ export default function Modules() {
           .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
-                <BsGripVertical className="me-2 fs-3" /> {module.name}{" "}
-                {/* {!module.editing && module.name} */}
+                <BsGripVertical className="me-2 fs-3" /> 
+                {!module.editing && module.name}
                 {module.editing && (
                   <input
                     className="form-control w-50 d-inline-block"
