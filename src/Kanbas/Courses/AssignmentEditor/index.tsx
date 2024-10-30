722eea904e38ece138af7b4205c387c5f9ba8788
VivianDongChen <chen.dong1@northeastern.edu>
2024-10-30 16:32:08 -0700

    1

--- src/Kanbas/Courses/AssignmentEditor/index.tsx
+++ src/Kanbas/Courses/AssignmentEditor/index.tsx
@@ -24,23 +24,7 @@
 //       </div>
 //       <br />
 
-//       {/* Instructions */}
-//       <div className="form-group">
-//         <label>Instructions</label>
-//         <div className="border p-3">
-//           <p>The assignment is <span style={{ color: "red" }}>available online</span></p>
-//           <p>Submit a link to the landing page of your Web application running on <span style={{ color: "black" }}>Netlify</span>.</p>
-//           <p>The landing page should include the following:</p>
-//           <ul>
-//             <li>Your full name and section</li>
-//             <li>Links to each of the lab assignments</li>
-//             <li>Link to the <span style={{ color: "black" }}>Kanbas</span> application</li>
-//             <li>Links to all relevant source code repositories</li>
-//           </ul>
-//           <p>The <span style={{ color: "black" }}>Kanbas</span> application should include a link to navigate back to the landing page.</p>
-//         </div>
-//       </div>
-//       <br />
+
 
 //       {/* Points */}
 //       <div className="form-group row">
@@ -51,97 +35,13 @@
 //       </div>
 //       <br />
 
-//       {/* Assignment Group */}
-//       <div className="form-group row">
-//         <label className="col-sm-2 col-form-label text-end">Assignment Group</label>
-//         <div className="col-sm-10">
-//           <select className="form-select">
-//             <option>ASSIGNMENT</option>
-//             <option>Quizzes</option>
-//             <option>Labs</option>
-//             <option>Projects</option>
-//           </select>
-//         </div>
-//       </div>
-//       <br />
 
-//       {/* Display Grade as */}
-//       <div className="form-group row">
-//         <label className="col-sm-2 col-form-label text-end">Display Grade as</label>
-//         <div className="col-sm-10">
-//           <select className="form-select">
-//             <option>Percentage</option>
-//             <option>Points</option>
-//             <option>Complete/Incomplete</option>
-//             <option>Letter Grade</option>
-//           </select>
-//         </div>
-//       </div>
-//       <br />
 
-//       {/* Submission Type */}
-//       <div className="form-group row">
-//         <label className="col-sm-2 col-form-label text-end">Submission Type</label>
-//         <div className="col-sm-10">
-//           <div className="border p-3">
-//             <select className="form-select">
-//               <option>Online</option>
-//               <option>In-person</option>
-//             </select>
-//             <br />
-//             <label>Online Entry Options</label>
-//             <div>
-//               <div className="form-check">
-//                 <input type="checkbox" className="form-check-input" id="websiteUrl" />
-//                 <label className="form-check-label" htmlFor="websiteUrl">Text Entry</label>
-//               </div>
-//               <div className="form-check">
-//                 <input type="checkbox" className="form-check-input" id="mediaRecordings" defaultChecked />
-//                 <label className="form-check-label" htmlFor="mediaRecordings">Website URL</label>
-//               </div>
-//               <div className="form-check">
-//                 <input type="checkbox" className="form-check-input" id="studentAnnotation" />
-//                 <label className="form-check-label" htmlFor="studentAnnotation">Media Recordings</label>
-//               </div>
-//               <div className="form-check">
-//                 <input type="checkbox" className="form-check-input" id="fileUploads" />
-//                 <label className="form-check-label" htmlFor="fileUploads">Student Annotation</label>
-//               </div>
-//               <div className="form-check">
-//                 <input type="checkbox" className="form-check-input" id="fileUploads2" />
-//                 <label className="form-check-label" htmlFor="fileUploads2">File Uploads</label>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <br />
+      
 
-//       {/* Assign to */}
-//       <div className="form-group row">
-//         <label className="col-sm-2 col-form-label text-end">Assign</label>
-//         <div className="col-sm-10 border p-3">
-//           <div className="form-group">
-//             <label>Assign to</label>
-//             <select className="form-control"></select>
-//           </div>
-//           <div className="form-group">
-//             <label>Due</label>
-//             <input type="datetime-local" className="form-control" defaultValue={assignment.due_date} />
-//           </div>
-//           <div className="row">
-//             <div className="col">
-//               <label>Available from</label>
-//               <input type="datetime-local" className="form-control" defaultValue={assignment.available_date} />
-//             </div>
-//             <div className="col">
-//               <label>Until</label>
-//               <input type="datetime-local" className="form-control" />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <br />
+
+
+
 
 //       {/* Action Buttons */}
 //       <hr />
@@ -220,6 +120,24 @@ export default function AssignmentEditor() {
       </div>
       <br />
 
+      {/* Instructions */}
+       <div className="form-group">
+         <label>Instructions</label>
+         <div className="border p-3">
+           <p>The assignment is <span style={{ color: "red" }}>available online</span></p>
+           <p>Submit a link to the landing page of your Web application running on <span style={{ color: "black" }}>Netlify</span>.</p>
+           <p>The landing page should include the following:</p>
+           <ul>
+             <li>Your full name and section</li>
+             <li>Links to each of the lab assignments</li>
+             <li>Link to the <span style={{ color: "black" }}>Kanbas</span> application</li>
+             <li>Links to all relevant source code repositories</li>
+          </ul>
+           <p>The <span style={{ color: "black" }}>Kanbas</span> application should include a link to navigate back to the landing page.</p>
+         </div>
+       </div>
+       <br />
+
       {/* Points */}
       <div className="form-group row">
         <label className="col-sm-2 col-form-label text-end">Points</label>
@@ -235,32 +153,100 @@ export default function AssignmentEditor() {
       </div>
       <br />
 
-      {/* Available Date */}
+      {/* Assignment Group */}
       <div className="form-group row">
-        <label className="col-sm-2 col-form-label text-end">Available From</label>
+        <label className="col-sm-2 col-form-label text-end">Assignment Group</label>
+         <div className="col-sm-10">
+           <select className="form-select">
+             <option>ASSIGNMENT</option>
+             <option>Quizzes</option>
+             <option>Labs</option>
+             <option>Projects</option>
+           </select>
+         </div>
+       </div>
+       <br />
+
+       {/* Display Grade as */}
+      <div className="form-group row">
+        <label className="col-sm-2 col-form-label text-end">Display Grade as</label>
         <div className="col-sm-10">
-          <input
-            type="datetime-local"
-            name="available_date"
-            className="form-control"
-            value={assignmentData.available_date}
-            onChange={handleChange}
-          />
+          <select className="form-select">
+            <option>Percentage</option>
+            <option>Points</option>
+            <option>Complete/Incomplete</option>
+            <option>Letter Grade</option>
+          </select>
         </div>
       </div>
       <br />
 
-      {/* Due Date */}
-      <div className="form-group row">
-        <label className="col-sm-2 col-form-label text-end">Due Date</label>
+      {/* Submission Type */}
+            <div className="form-group row">
+        <label className="col-sm-2 col-form-label text-end">Submission Type</label>
         <div className="col-sm-10">
-          <input
-            type="datetime-local"
-            name="due_date"
-            className="form-control"
-            value={assignmentData.due_date}
-            onChange={handleChange}
-          />
+          <div className="border p-3">
+            <select className="form-select">
+              <option>Online</option>
+              <option>In-person</option>
+            </select>
+            <br />
+            <label>Online Entry Options</label>
+            <div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="websiteUrl" />
+                <label className="form-check-label" htmlFor="websiteUrl">Text Entry</label>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="mediaRecordings" defaultChecked />
+                <label className="form-check-label" htmlFor="mediaRecordings">Website URL</label>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="studentAnnotation" />
+                <label className="form-check-label" htmlFor="studentAnnotation">Media Recordings</label>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="fileUploads" />
+                <label className="form-check-label" htmlFor="fileUploads">Student Annotation</label>
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="fileUploads2" />
+                <label className="form-check-label" htmlFor="fileUploads2">File Uploads</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+
+      {/* Assign to */}
+            <div className="form-group row">
+        <label className="col-sm-2 col-form-label text-end">Assign</label>
+        <div className="col-sm-10 border p-3">
+          <div className="form-group">
+            <label>Assign to</label>
+            <select className="form-control"></select>
+          </div>
+
+          <div className="form-group">
+            <label>Due</label>
+            <input type="datetime-local" className="form-control" 
+            defaultValue={assignmentData.due_date} onChange={handleChange}/>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              <label>Available from</label>
+              <input type="datetime-local" className="form-control" 
+              defaultValue={assignmentData.available_date} onChange={handleChange}/>
+            </div>
+
+            
+            <div className="col">
+              <label>Until</label>
+              <input type="datetime-local" className="form-control" />
+            </div>
+          </div>
         </div>
       </div>
       <br />
