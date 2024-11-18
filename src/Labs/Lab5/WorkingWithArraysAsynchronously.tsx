fe822d398d0714f5c9ed47b3b593c78389972e71
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-17 21:36:45 -0800

    1

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -65,6 +65,14 @@ export default function WorkingWithArraysAsynchronously() {
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
+      {errorMessage && (
+        <div
+          id="wd-todo-error-message"
+          className="alert alert-danger mb-2 mt-2"
+        >
+          {errorMessage}
+        </div>
+      )}
       <h4>
         Todos
         <FaPlusCircle
