b4819109f585015355021c2bae40588081f23b6d
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-18 17:45:48 -0800

    1

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -6,10 +6,15 @@ export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const signin = async (credentials: any) => {
   //The credentials object is sent as the request body.
   const response = await axios.post(`${USERS_API}/signin`, credentials); //Awaits the server's response.
-  return response.data;//The function returns the data received from the server (response.data).
+  return response.data; //The function returns the data received from the server (response.data).
 };
 
 export const signup = async (user: any) => {
   const response = await axios.post(`${USERS_API}/signup`, user);
   return response.data;
-  };
+};
+
+export const updateUser = async (user: any) => {
+  const response = await axios.put(`${USERS_API}/${user._id}`, user);
+  return response.data;
+};
