bb9082ea4490768c636018b995ecaa29587084cb
VivianDongChen <chen.dong1@northeastern.edu>
2024-12-05 12:26:03 -0800

    5.1.2 Inserting Courses into a Database

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -5,18 +5,26 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+// Retrieving Courses from a Database
 export const fetchAllCourses = async () => {
   const { data } = await axiosWithCredentials.get(COURSES_API);
   return data;
 };
 
+// Inserting Courses into a Database
 export const createCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.post(
-    `${USERS_API}/current/courses`,
+    // `${USERS_API}/current/courses`,
+    COURSES_API,
     course
   );
   return data;
 };
+
+
+
+
+
 
 export const deleteCourse = async (id: string) => {
   const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
